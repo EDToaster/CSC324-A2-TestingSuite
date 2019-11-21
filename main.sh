c6a0b89544767dda9818fec3da51e126d12e93b0
EDToaster <elongateddanishtoaster@gmail.com>
2019-11-20 20:44:55 -0500

    Fixed main.sh: now with 200% more comments!

--- main.sh
+++ main.sh
@@ -4,27 +4,30 @@ rm -rf ./tmp
 mkdir ./tmp
 
 for file in `ls ./tests`; do
-
   PROGRAM=`tr -cd "[:print:]\n" < "./tests/$file"`
-  HASKELL=`racket transpiler.rkt "$PROGRAM"`
 
+  # headers for the language
   echo "#!/usr/bin/racket" > ./tmp/original.rkt
   echo "#lang racket" >> ./tmp/original.rkt
 
+  # both files need this header
   cat ./tmp/original.rkt > ./tmp/transformed.rkt
 
-  echo "(require racket/control)" >> ./tmp/original.rkt
-  echo "(define (cps:+ . args) (apply + args))" >> ./tmp/original.rkt
-  echo "(define (cps:* . args) (apply * args))" >> ./tmp/original.rkt
-  echo "(define (cps:equal? . args) (apply equal? args))" >> ./tmp/original.rkt
-  echo "(define (is-exception-of? msg) (lambda (error) (equal? msg error)))" >> ./tmp/original.rkt
-  echo "(define-syntax try
-        (syntax-rules ()
-          ((_ expr msg handler)
-          (with-handlers ([(is-exception-of? msg) (lambda (err) (handler))]) expr))))" >> ./tmp/original.rkt
+  # original racket header and program -- needed for evaulation of the original program input
+  echo "(require racket/control)
+        (define (cps:+ . args) (apply + args))
+        (define (cps:* . args) (apply * args))
+        (define (cps:equal? . args) (apply equal? args))
+        (define (is-exception-of? msg) (lambda (error) (equal? msg error)))
+        (define-syntax try
+              (syntax-rules ()
+                ((_ expr msg handler)
+                (with-handlers ([(is-exception-of? msg) (lambda (err) (handler))]) expr))))
+        $PROGRAM" >> ./tmp/original.rkt
 
-  echo "$PROGRAM" >> ./tmp/original.rkt
-
+  # transpile to haskell syntax
+  HASKELL=`racket transpiler.rkt "$PROGRAM"`
+  # evaulate the haskell version in ghci, to get cpsTransformed racket
   echo "import Control.Monad
         import System.IO
         (forM_ [stdout, stderr] . flip hPutStrLn) $ show $ cpsTransformProgS $ (Prog $HASKELL)" | ghci -i.. Chups 2>> "./tmp/transformed.rkt" 1>/dev/null
